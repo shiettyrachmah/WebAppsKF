697153797ab2a935c1c57653172c62e5aa9269dd
shiettyrachmah <shiettyrachmah@gmail.com>
2023-10-30 16:55:46 +0700

    Add project files.

---
+++ Controllers/StudyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppsKF.Controllers
+{
+    public class StudyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppsKF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
